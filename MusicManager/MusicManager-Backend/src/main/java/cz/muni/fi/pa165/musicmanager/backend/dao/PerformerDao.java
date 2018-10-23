dd8825a46b6eca49ff1c300288c0fd85488cb2b6
Marek Bařinka <xbarinka@fi.muni.cz>
2018-10-23 11:13:44 +0200

    Created initial folder structure

---
+++ MusicManager/MusicManager-Backend/src/main/java/cz/muni/fi/pa165/musicmanager/backend/dao/PerformerDao.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.musicmanager.backend.dao;
+
+/**
+ *
+ * @author Marek Barinka; 456295
+ */
+public interface PerformerDao {
+    
+}
---
+++ MusicManager/MusicManager-Backend/src/main/java/cz/muni/fi/pa165/musicmanager/backend/entity/Performer.java
@@ -0,0 +1,9 @@
+package cz.muni.fi.pa165.musicmanager.backend.entity;
+
+/**
+ *
+ * @author Marek Barinka; 456295
+ */
+public class Performer {
+    
+}
