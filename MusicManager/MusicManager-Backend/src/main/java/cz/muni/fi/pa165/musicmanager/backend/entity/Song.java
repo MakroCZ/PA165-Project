a56748eb91cda0aef34986c568f839efa7a13ad3
Lukáš Suchánek <xsuchan4@fi.muni.cz>
2018-10-27 15:33:08 +0200

    Song redone

--- MusicManager/MusicManager-Backend/src/main/java/cz/muni/fi/pa165/musicmanager/backend/entity/Song.java
+++ MusicManager/MusicManager-Backend/src/main/java/cz/muni/fi/pa165/musicmanager/backend/entity/Song.java
@@ -31,8 +31,8 @@ public class Song {
     @ManyToOne(optional = false)
     private Album album;
 
-    @ManyToMany(mappedBy="songs")
-    private Set<Genre> genres;
+    @ManyToOne(optional = false)
+    private Genre genre;
 
     public Song(String name, LocalTime length, LocalDate date){
         this.setName(name);
@@ -82,12 +82,12 @@ public class Song {
         this.album = album;
     }
 
-    public Set<Genre> getGenres() {
-        return Collections.unmodifiableSet(genres);
+    public Genre getGenre() {
+        return genre;
     }
 
-    public void setGenres(Set<Genre> genres) {
-        this.genres = genres;
+    public void setGenre(Genre genre) {
+        this.genre = genre;
     }
 
     public String toString(){
