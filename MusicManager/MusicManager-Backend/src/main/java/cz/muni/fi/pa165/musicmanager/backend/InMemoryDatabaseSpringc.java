6ed23bbf1cff3e6b56cee0f282561a4e26530c8a
Marek <marek.barinka@gmail.com>
2018-10-27 19:48:55 +0200

    Added application configuration (application context and in memory db)

---
+++ MusicManager/MusicManager-Backend/src/main/java/cz/muni/fi/pa165/musicmanager/backend/InMemoryDatabaseSpringc.java
@@ -0,0 +1,31 @@
+package cz.muni.fi.pa165.musicmanager.backend;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+
+
+@Configuration
+public class InMemoryDatabaseSpringc {
+
+	@Bean
+	public DataSource db(){
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder.setType(EmbeddedDatabaseType.DERBY).build();
+		return db;
+	}
+}
