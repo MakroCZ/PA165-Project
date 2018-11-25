77ec037b4295b63af3f432fa6ee0cfe6edd79474
Marek <marek.barinka@gmail.com>
2018-11-25 10:00:36 +0100

    Implemented Performer facade

--- MusicManager/MusicManager-API/src/main/java/cz/muni/fi/pa165/mm/api/facade/PerformerFacade.java
+++ MusicManager/MusicManager-API/src/main/java/cz/muni/fi/pa165/mm/api/facade/PerformerFacade.java
@@ -1,9 +1,45 @@
 package cz.muni.fi.pa165.mm.api.facade;
 
+import cz.muni.fi.pa165.mm.api.dto.PerformerCreateDTO;
+import cz.muni.fi.pa165.mm.api.dto.PerformerDTO;
+import java.util.List;
+
 /**
  *
  * @author Marek Barinka; 456295
  */
 public interface PerformerFacade {
     
+    /**
+     * Add performer to database
+     * @param p Performer to add
+     */
+    void create(PerformerCreateDTO p);
+    
+    /**
+     * Finds all stored performers
+     * @return List containing all stored performers,
+     * empty list if no performers are stored
+     */
+    List<PerformerDTO> findAll();
+    
+    /**
+     * Finds one performer with specified id
+     * @param id Id of perfomer to find
+     * @return Performer with specified id,
+     * null if there is no performer with specified id
+     */
+    PerformerDTO findById(Long id);
+    
+    /**
+     * Updates performer
+     * @param p Performer to update
+     */
+    void update(PerformerDTO p);
+    
+    /**
+     * Removes performer
+     * @param id Id of performer to remove
+     */
+    void remove(Long id);
 }
