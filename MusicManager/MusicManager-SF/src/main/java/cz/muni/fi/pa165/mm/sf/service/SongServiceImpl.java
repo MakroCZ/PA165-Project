74ea7d999b58ea4ec2620507835d9b2a977244c8
Lukáš Suchánek <xsuchan4@fi.muni.cz>
2018-11-26 19:07:01 +0100

    song service remove validation

--- MusicManager/MusicManager-SF/src/main/java/cz/muni/fi/pa165/mm/sf/service/SongServiceImpl.java
+++ MusicManager/MusicManager-SF/src/main/java/cz/muni/fi/pa165/mm/sf/service/SongServiceImpl.java
@@ -22,14 +22,12 @@ public class SongServiceImpl implements SongService{
 
     @Override
     public Song create(Song song){
-        validateSong(song);
         songDao.create(song);
         return song;
     }
     @Override
     public void delete(Song song){
 
-        validateSong(song);
         if(song.getId()== null){
             throw new IllegalArgumentException("Song id can't be null");
         }
@@ -37,7 +35,6 @@ public class SongServiceImpl implements SongService{
     }
     @Override
     public void update(Song song){
-        validateSong(song);
         if(song.getId()== null){
             throw new IllegalArgumentException("Song id can't be null");
         }
@@ -64,23 +61,5 @@ public class SongServiceImpl implements SongService{
             songs.addAll(album.getSongs());
         }
         return songs;
-    }
-
-    private static void validateSong(Song song){
-        if(song == null){
-            throw new IllegalArgumentException("Song is null");
-        }
-        if (song.getName() == null){
-            throw new IllegalArgumentException("name is null");
-        }
-        if(song.getLength() == null){
-            throw new IllegalArgumentException("length is null");
-        }
-        if(song.getDate() == null){
-            throw new IllegalArgumentException("date is null");
-        }
-        if(song.getDate().isAfter(LocalDate.now()) ){
-            throw new IllegalArgumentException("Date can't be in the future");
-        }
     }
 }
