5b3eaf5c2f1e4ca2cab3d2b42664d2154f97a237
ysafonov <487596@mail.muni.cz>
2018-11-26 22:33:17 +0100

    Service layer tests implementation and facade tests implementation

--- MusicManager/MusicManager-SF/src/test/java/cz/nubi/fi/pa165/mm/sf/SongFacadeTest.java
+++ MusicManager/MusicManager-SF/src/test/java/cz/nubi/fi/pa165/mm/sf/SongFacadeTest.java
@@ -1,32 +1,193 @@
 package cz.nubi.fi.pa165.mm.sf;
 
+import cz.muni.fi.pa165.mm.api.dto.AlbumDTO;
+import cz.muni.fi.pa165.mm.api.dto.GenreDTO;
+import cz.muni.fi.pa165.mm.api.dto.SongCreateDTO;
+import cz.muni.fi.pa165.mm.api.dto.SongDTO;
 import cz.muni.fi.pa165.mm.api.facade.SongFacade;
-import cz.muni.fi.pa165.mm.sf.service.config.ServiceConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import cz.muni.fi.pa165.mm.daolayer.entity.Album;
+import cz.muni.fi.pa165.mm.daolayer.entity.Genre;
+import cz.muni.fi.pa165.mm.daolayer.entity.Song;
+import cz.muni.fi.pa165.mm.sf.facade.SongFacadeImpl;
+import cz.muni.fi.pa165.mm.sf.service.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This java class represents unit tests for the SongFacate class
  * @author Yehor Safonov; 487596
  */
 
-@ContextConfiguration(classes = ServiceConfiguration.class)
-public class SongFacadeTest extends AbstractTestNGSpringContextTests {
+public class SongFacadeTest  {
 
-    @Autowired
-    private SongFacade songFacade;
+    @Mock
+    private SongService songService;
+
+    @Mock
+    private AlbumService albumService;
+
+    @Mock
+    private GenreService genreService;
+
+    @Mock
+    private BeanMappingService beanMappingService;
+
+
+    @InjectMocks
+    private SongFacade songFacade = new SongFacadeImpl();
+
+    private SongDTO songDTO;
+    private GenreDTO genreDTO;
+    private Song song;
+    private Album album;
+
+    @BeforeClass
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+
+        /*
+            Creating a genreDTODTO and setting its parametrs
+        */
+
+        genreDTO = new GenreDTO();
+        genreDTO.setId(1L);
+        genreDTO.setName("Rock");
+        genreDTO.setDescription("Rock music is a broad genreDTO of popular music that originated as" +
+                " \"rock and roll\" in the United States in the early 1950s, and developed into" +
+                " a range of different styles in the 1960s and later, particularly in the United" +
+                " Kingdom and in the United States");
+
+         /*
+             Creating an albumDTO and setting its parametrs
+         */
+
+        AlbumDTO albumDTO = new AlbumDTO();
+        albumDTO.setId(2L);
+        albumDTO.setDate(LocalDate.of(2015, Month.APRIL, 16));
+        albumDTO.setName("Final masquerade");
+
+
+        album = new Album();
+        album.setId(albumDTO.getId());
+        album.setDate(albumDTO.getDate());
+        album.setName(albumDTO.getName());
+
+        songDTO = new SongDTO();
+        songDTO.setId(3L);
+        songDTO.setName("In the end");
+        songDTO.setAlbum(albumDTO);
+        songDTO.setGenre(genreDTO);
+        songDTO.setLength(LocalTime.of(0,4,45));
+        songDTO.setDate(LocalDate.of(2014, Month.MAY, 10));
+    }
+
+    @AfterMethod
+    void reset(){
+        Mockito.reset(songService);
+        Mockito.reset(genreService);
+        Mockito.reset(albumService);
+        Mockito.reset(beanMappingService);
+    }
 
     @Test
-    public  void  testCreateSong(){}
+    public  void  testCreateSong(){
+        SongCreateDTO songCreateDTO = new SongCreateDTO();
+        songCreateDTO.setName(songDTO.getName());
+        songCreateDTO.setDate(songDTO.getDate());
+        songCreateDTO.setLength(songDTO.getLength());
+        songCreateDTO.setAlbumId(album.getId());
+        songCreateDTO.setGenreId(genreDTO.getId());
+
+
+        Genre genre = new Genre();
+        genre.setId(genreDTO.getId());
+        genre.setName(genreDTO.getName());
+        genre.setDescription(genreDTO.getDescription());
+
+        song = new Song();
+        song.setId(songDTO.getId());
+        song.setName(songDTO.getName());
+        song.setDate(songDTO.getDate());
+        song.setLength(songDTO.getLength());
+        song.setGenre(genre);
+        song.setAlbum(album);
+
+        Mockito.doReturn(songCreateDTO).when(beanMappingService).mapTo(song, SongCreateDTO.class);
+        Mockito.doReturn(song).when(beanMappingService).mapTo(songCreateDTO, Song.class);
+
+        Mockito.doReturn(album).when(albumService).retrieve(any(Long.class));
+        Mockito.doReturn(genre).when(genreService).findById(any(Long.class));
+        Mockito.doReturn(song).when(songService).create(any());
+
+        Long id = songFacade.createSong(songCreateDTO);
+        Mockito.verify(songService).create(song);
+
+        Assert.assertEquals(id, song.getId());
+    }
 
     @Test
-    public  void  testDeleteSong(){}
+    public  void  testDeleteSong(){
+        song = new Song();
+        song.setId(songDTO.getId());
+        song.setName(songDTO.getName());
+        song.setLength(songDTO.getLength());
+        song.setDate(songDTO.getDate());
+
+        Mockito.doNothing().when(songService).delete(any(Song.class));
+        songFacade.deleteSong(songDTO.getId());
+        verify(songService).delete(any(Song.class));
+    }
 
     @Test
-    public  void testGetAllSongs(){}
+    public  void testGetAllSongs(){
+        Song test_song = new Song();
+        test_song.setName("Somewhere I belong");
+        test_song.setLength(LocalTime.of(0,3,45));
+        test_song.setDate(LocalDate.of(2015, Month.JANUARY, 12));
+
+        List<Song> songs = new ArrayList<>();
+        songs.add(test_song);
+
+        List<SongDTO> songsDTO = new ArrayList<>();
+        songsDTO.add(songDTO);
+
+        Mockito.when( beanMappingService.mapTo(songs, SongDTO.class)).thenReturn(songsDTO);
+        Mockito.when(songService.findAll()).thenReturn(songs);
+        List<SongDTO> foundSongsDTO = songFacade.getAllSongs();
+        Mockito.verify(songService).findAll();
+        Assert.assertEquals(foundSongsDTO.size(), songs.size());
+    }
 
     @Test
-    public void testGetSongWithID(){}
+    public void testGetSongWithID(){
+       Song test_song = new Song();
+       test_song.setName("Somewhere I belong");
+       test_song.setLength(LocalTime.of(0,3,45));
+       test_song.setDate(LocalDate.of(2015, Month.JANUARY, 12));
+
+       Mockito.doReturn(test_song).when(songService).findById(4L);
+       Mockito.doReturn(songDTO).when(beanMappingService).mapTo(test_song, SongDTO.class);
+       Mockito.doReturn(songDTO).when(beanMappingService).mapTo(test_song, SongDTO.class);
+       SongDTO songDTO1 = songFacade.getSongWithID(4L);
+       Assert.assertEquals(songDTO, songDTO1);
+    }
 }
--- MusicManager/MusicManager-SF/src/test/java/cz/nubi/fi/pa165/mm/sf/SongServiceTest.java
+++ MusicManager/MusicManager-SF/src/test/java/cz/nubi/fi/pa165/mm/sf/SongServiceTest.java
@@ -1,70 +1,269 @@
 package cz.nubi.fi.pa165.mm.sf;
+
+
 import cz.muni.fi.pa165.mm.daolayer.dao.SongDao;
-import cz.muni.fi.pa165.mm.sf.service.GenreService;
+import cz.muni.fi.pa165.mm.daolayer.entity.Album;
+import cz.muni.fi.pa165.mm.daolayer.entity.Genre;
+import cz.muni.fi.pa165.mm.daolayer.entity.Performer;
+import cz.muni.fi.pa165.mm.daolayer.entity.Song;
+
 import cz.muni.fi.pa165.mm.sf.service.SongService;
-import cz.muni.fi.pa165.mm.sf.service.config.ServiceConfiguration;
+import cz.muni.fi.pa165.mm.sf.service.SongServiceImpl;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * This java class represents all tests for the SongService layer
  * @author Yehor Safonov; 487596
  */
 
-@ContextConfiguration(classes= ServiceConfiguration.class)
-public class SongServiceTest extends AbstractTransactionalTestNGSpringContextTests  {
+public class SongServiceTest {
+
     @Mock
     private SongDao songDao;
 
-    @Mock
-    private AlbumService albumService;
-
-    @Mock
-    private GenreService genreService;
-
-    @Autowired
     @InjectMocks
-    private SongService songService;
+    private SongService songService = new SongServiceImpl();
+
+    private Song song_InTheEnd;
+    private Song song_SomewhereIBelong;
+    private Song song_BurnItDown;
+    private Album albumLinkinkPark;
+    private Genre genreRock;
 
     @BeforeClass
-    public void setup() throws ServiceException
-    {
+    public void setup() throws ServiceException {
         MockitoAnnotations.initMocks(this);
     }
 
     @BeforeMethod
-    public void prepareTestSong(){}
+    public void beforeMethod() {
+        /*
+            Creating a genreRock and setting its parametrs
+        */
+        genreRock = new Genre();
+        genreRock.setName("Rock");
+        genreRock.setId(5L);
+        genreRock.setDescription("Rock music is a broad genreRock of popular music that originated as" +
+                " \"rock and roll\" in the United States in the early 1950s, and developed into" +
+                " a range of different styles in the 1960s and later, particularly in the United" +
+                " Kingdom and in the United States");
+
+         /*
+             Creating an albumLinkinkPark and setting its parametrs
+         */
+        albumLinkinkPark = new Album();
+        albumLinkinkPark.setId(4L);
+        albumLinkinkPark.setDate(LocalDate.of(2015, Month.APRIL, 16));
+        albumLinkinkPark.setName("Final masquerade");
+
+        /*
+             Creating a set of different songs and setting their parametrs
+        */
+        song_InTheEnd = new Song();
+        song_SomewhereIBelong = new Song();
+        song_BurnItDown = new Song();
+
+        song_InTheEnd.setId(1L);
+        song_InTheEnd.setName("In the end");
+        song_InTheEnd.setDate(LocalDate.of(2014, Month.MAY, 10));
+        song_InTheEnd.setAlbum(albumLinkinkPark);
+        song_InTheEnd.setGenre(genreRock);
+        song_InTheEnd.setLength(LocalTime.of(0,4,45));
+
+        song_SomewhereIBelong.setId(2L);
+        song_SomewhereIBelong.setName("Somewhere I belong");
+        song_SomewhereIBelong.setDate(LocalDate.of(2015, Month.JANUARY, 12));
+        song_SomewhereIBelong.setAlbum(albumLinkinkPark);
+        song_SomewhereIBelong.setGenre(genreRock);
+        song_SomewhereIBelong.setLength(LocalTime.of(0,3,45));
+
+        song_BurnItDown.setId(3L);
+        song_BurnItDown.setName("Burn it down");
+        song_BurnItDown.setDate(LocalDate.of(2014, Month.SEPTEMBER, 25));
+        song_BurnItDown.setAlbum(albumLinkinkPark);
+        song_BurnItDown.setGenre(genreRock);
+        song_BurnItDown.setLength(LocalTime.of(0,2,56));
+    }
 
     @Test
-    public  void  testDelete(){}
+    public void testDelete() {
+        /*
+             Try to delete song_InTheEnd and verfy if corresponding action is done
+        */
+        songService.delete(song_InTheEnd);
+        Mockito.verify(songDao, Mockito.times(1)).delete(song_InTheEnd);
+    }
+
 
     @Test
-    public void testCreate(){}
+    public void testCreate() {
+         /*
+             Creating a test song and setting its parametrs
+        */
+        Song testSong = new Song();
+        testSong.setId(6L);
+        testSong.setName("In the end test");
+        testSong.setDate(LocalDate.of(2017, Month.MAY, 10));
+        testSong.setAlbum(albumLinkinkPark);
+        testSong.setGenre(genreRock);
+        testSong.setLength(LocalTime.of(0,2,45));
+
+         /*
+             Try to create testSong and verfy if corresponding action is done
+        */
+        songService.create(testSong);
+        Mockito.verify(songDao).create(testSong);
+    }
 
     @Test
-    public void testUpdate(){}
+    public void testUpdate() {
+        /*
+             Try to update the song Castle of Glass information
+        */
+        song_InTheEnd.setName("New name");
+        song_InTheEnd.setDate(LocalDate.of(2012, Month.MARCH, 5));
+
+         /*
+             Try to update song_InTheEnd and verfy if corresponding action is done
+        */
+        songService.update(song_InTheEnd);
+        Mockito.verify(songDao).update(song_InTheEnd);
+    }
+
 
     @Test
-    public void testCreateNull(){}
+    public void testFindAll() {
+         /*
+             Try to find all Songs witch are presented
+        */
+        List<Song> allSongs = new ArrayList<>();
+        allSongs.add(song_InTheEnd);
+        allSongs.add(song_SomewhereIBelong);
+        allSongs.add(song_BurnItDown);
+
+        Mockito.when(songDao.findAll()).thenReturn(allSongs);
+        List<Song> foundSongs = songService.findAll();
+
+        Assert.assertEquals(allSongs.size(),foundSongs.size());
+    }
 
     @Test
-    public void testFindAll(){}
+    public void findById() {
+        /*
+             Try to find all Songs with specific IDs
+        */
+        Mockito.when(songDao.findById(song_InTheEnd.getId())).thenReturn(song_InTheEnd);
+        Mockito.when(songDao.findById(song_SomewhereIBelong.getId())).thenReturn(song_SomewhereIBelong);
+        Mockito.when(songDao.findById(song_BurnItDown.getId())).thenReturn(song_BurnItDown);
+
+        Song song_is_InTheEnd = songService.findById(song_InTheEnd.getId());
+        Song song_is_SomewhereIBelong = songService.findById(song_SomewhereIBelong.getId());
+        Song song_is_BurnItDown = songService.findById(song_BurnItDown.getId());
+
+        /*
+             Verify if songs are the same
+        */
+        Assert.assertEquals(song_is_InTheEnd, song_InTheEnd);
+        Assert.assertEquals(song_is_SomewhereIBelong, song_SomewhereIBelong);
+        Assert.assertEquals(song_is_BurnItDown, song_BurnItDown);
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testFindAllNullSongsFromSamePerformer() {
+        /*
+             Try to find all songs if performer is argument is null
+        */
+        songService.findAllSongsFromSamePerformer(null);
+    }
+
 
     @Test
-    public void findById(){}
+    public void testFindAllSongsFromSamePerformerIfEmptySongs() {
+        /*
+            Tests if returns the right array during calling the business function
+        */
+        Performer performer = new Performer();
+        performer.setName("Daniel Benningn");
+        performer.addAlbum(albumLinkinkPark);
+        List<Song> songs = new ArrayList<>();
+        Mockito.when(songDao.findAll()).thenReturn(songs);
+        List<Song> songsOfThePerformer = songService.findAllSongsFromSamePerformer(song_InTheEnd);
+        Assert.assertEquals(songsOfThePerformer.size(), albumLinkinkPark.getSongs().size());
+    }
+
+    @Test(expectedExceptions = DataAccessException.class)
+    void testUpdateNull(){
+        /*
+            Tests if appears DataAccessException in case of updating null song
+        */
+        Mockito.doThrow(InvalidDataAccessApiUsageException.class).when(songDao).update(null);
+        songService.update(null);
+    }
+
+    @Test(expectedExceptions = DataAccessException.class)
+    void testCreateNull(){
+          /*
+            Tests if appears DataAccessException in case of create null song
+        */
+        Mockito.doThrow(InvalidDataAccessApiUsageException.class).when(songDao).create(null);
+        songService.create(null);
+    }
 
     @Test
-    public void testFindAllSongsFromSamePerformer(){}
+    public void testFindAllSongsFromSamePerformer() {
+        /*
+             Create new album
+        */
+        Album albumMetallica = new Album();
+        albumMetallica.setId(5L);
+        albumMetallica.setDate(LocalDate.of(2016, Month.APRIL, 16));
+        albumMetallica.setName("Final masquerade");
 
-    @Test
-    public void testFindAllNullSongsFromSamePerformer(){}
+        /*
+            Create a new song that has different performer
+        */
+        Song song_papercut =  new Song();
+        song_papercut.setId(3L);
+        song_papercut.setName("Papercut");
+        song_papercut.setDate(LocalDate.of(2014, Month.SEPTEMBER, 25));
+        song_papercut.setAlbum(albumMetallica);
+        song_papercut.setGenre(genreRock);
+        song_papercut.setLength(LocalTime.of(0,2,56));
 
+        /*
+             Create new performer
+        */
+        Performer performer = new Performer();
+        performer.setName("Chester Bennington");
+        performer.addAlbum(albumLinkinkPark);
 
+        /*
+             Create a list of all songs
+        */
+        List<Song> allSongs = new ArrayList<>();
+        allSongs.add(song_InTheEnd);
+        allSongs.add(song_SomewhereIBelong);
+        allSongs.add(song_BurnItDown);
+        allSongs.add(song_papercut);
+
+        List<Song> songsOfThePerformer =  new ArrayList<>();
+        Mockito.when(songDao.findAll()).thenReturn(allSongs);
+        songsOfThePerformer =  songService.findAllSongsFromSamePerformer(song_InTheEnd);
+        Assert.assertNotEquals(songsOfThePerformer, allSongs);
+        songsOfThePerformer =  songService.findAllSongsFromSamePerformer(song_SomewhereIBelong);
+        Assert.assertNotEquals(songsOfThePerformer, allSongs);
+    }
 }
