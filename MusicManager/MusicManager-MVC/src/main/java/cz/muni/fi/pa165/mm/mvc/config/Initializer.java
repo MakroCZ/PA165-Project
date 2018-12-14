992e281735bd4aeea1692b1a6f6f4cc1174191a9
ysafonov <487596@mail.muni.cz>
2018-12-14 16:20:52 +0100

    Initializer Configuration MVC

---
+++ MusicManager/MusicManager-MVC/src/main/java/cz/muni/fi/pa165/mm/mvc/config/Initializer.java
@@ -0,0 +1,31 @@
+package cz.muni.fi.pa165.mm.mvc.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class Initializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{SpringMvcConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("utf-8");
+        return new Filter[]{encodingFilter};
+    }
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+}
---
+++ MusicManager/MusicManager-MVC/src/main/java/cz/muni/fi/pa165/mm/mvc/controllers/GenreController.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.mm.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GenreController {
+
+    @RequestMapping(value = "/")
+    public String home(){
+        return "test";
+    }
+
+}
