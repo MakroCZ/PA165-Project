158a381a62f86d448e7f10ce0495d0a577b7a247
Marek <marek.barinka@gmail.com>
2018-12-17 12:32:11 +0100

    Logging system update

--- MusicManager/MusicManager-SF/src/main/java/cz/muni/fi/pa165/mm/sf/service/UserServiceImpl.java
+++ MusicManager/MusicManager-SF/src/main/java/cz/muni/fi/pa165/mm/sf/service/UserServiceImpl.java
@@ -4,16 +4,27 @@ import cz.muni.fi.pa165.mm.daolayer.dao.UserDao;
 import cz.muni.fi.pa165.mm.daolayer.entity.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author Marek Barinka; 456295
  */
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDao userDao;
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
+    @Override
+    public void registerUser(User u, String passwd) {
+        u.setPasswordHash(passwordEncoder.encode(passwd));
+        userDao.create(u);
+    }
     
     @Override
     public List<User> findAllUsers() {
@@ -22,7 +33,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean authenticate(User u, String password) {
-        return BCrypt.checkpw(password, u.getPasswordHash());
+        return passwordEncoder.encode(password).equals(u.getPasswordHash());
     }
 
     @Override
